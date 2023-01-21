33b19bc7399f039ccd341ad1da79672557b5c512
Devanshi <devanshipujara02@gmail.com>
2023-01-21 14:48:57 +0530

    Added two types of filters and sort button

--- src/App.js
+++ src/App.js
@@ -5,24 +5,42 @@ import items from './Data.js';
 // const allCategories = ;
 
 function App() {
-  const [activeCategory,setactiveCategory] = useState("All");
+  const [activefilter1category,setactivefilter1category] = useState("All");
+  const [activefilter2category,setactivefilter2category] = useState("All");
+  const [activesortcategory,setactivesortcategory]=useState("None");
   const [menuitems, setmenuitems] = useState(items);
 
-  const categories=["All", ...new Set(items.map((item) => item.category))];
+  const filter1categories=["All", ...new Set(items.map((item) => item.category1))];
+  const filter2categories=["All", ...new Set(items.map((item) => item.category2))];
+  
+  const sortcategories=["None","Price","Popularity"];
   
 
-  const filteritems = (category)=> {
-    // console.log(`filtering items by category ${category}`)
-    setactiveCategory(category);
-    if(category === "All"){
-      setmenuitems(items);
-    }
-    else{
-      const newitems=items.filter((item) => item.category === category);
-      setmenuitems(newitems);
+  const filteritems = (category1,category2)=> {
+    setactivefilter1category(category1);
+    setactivefilter2category(category2);
+
+    var newitems=items;
+
+    if(category1!=="All"){
+      newitems=newitems.filter((item) => item.category1 === category1);
     }
 
+    if(category2!="All"){
+      newitems=newitems.filter((item) => item.category2 === category2);
+    }
+
+    setmenuitems(newitems);
   }
+
+  //how to loop through key value pairs in an object
+  // const showsortcategories = () =>{
+  //   Object.entries(items[0]).map(([key, value]) => {
+  //     if(Number.isInteger(value)){
+  //       console.log(key);
+  //     }
+  //   })
+  // }
 
   return (
     <div className="App">
@@ -30,19 +48,83 @@ function App() {
       <h1>This is Jane's Kitchen</h1>
       </div>
       <div className='category_container'>
-        {categories.map((category) => {
+        {/* {filtercategories.map((category) => {
           return(
             <div className='buttonss'>
               <button
               type="button"
-              className={`${activeCategory === category?'filter-btn-active': 'filter-btn'}`}
+              className={`${activefiltercategory === category?'filter-btn-active': 'filter-btn'}`}
               onClick={() => filteritems(category)}>
               {category}</button>
             </div>
           );
-        })}
+        })} */}
+        <button className='filter-btn'>Menu</button>
+        <button className='filter-btn'>About</button>
+        <button className='filter-btn'>Contact</button>
       </div>
 
+      {/* <div className='dropdown'>
+
+        <div className='filterdiv'>
+          <button className='filter-dropbtn'>Filter</button>
+          <div className='dropdown-content'>
+            <button>Show all</button>
+            <button>Breakfast</button>
+            <button>Lunch</button>
+            <button>Dinner</button>
+          </div>
+        </div>
+
+        <div className='sortdiv'>
+          <button className='sort-dropbtn'>Sort</button>
+          <div className='dropdown-content'>
+            <button>Price</button>
+            <button>Popularity</button>
+          </div>
+        </div>
+
+      </div> */}
+
+
+      <div className='dropdown'>
+        <div className='filterdivhead'>Filter by: </div>
+        <div className='filterdiv'>
+          <button className='filter-dropbtn'>{activefilter1category}</button>
+          <div className='dropdown-content'>
+            {filter1categories.map((category) => {
+              return(
+                <button onClick={() => filteritems(category,activefilter2category)}>{category}</button>
+              )
+            })}
+          </div>
+        </div>
+
+        <div className='filterdiv'>
+          <button className='filter-dropbtn'>{activefilter2category}</button>
+          <div className='dropdown-content'>
+            {filter2categories.map((category) => {
+              return(
+                <button onClick={() => filteritems(activefilter1category,category)}>{category}</button>
+              )
+            })}
+          </div>
+        </div>
+
+        <div className='filterdivhead'>Sort by: </div>
+        <div className='sortdiv'>
+          <button className='sort-dropbtn'>Sort</button>
+          <div className='dropdown-content'>
+            {sortcategories.map((category) => {
+              return(
+                <button>{category}</button>
+              )
+            })}
+          </div>
+        </div>
+
+      </div>
+      
       {/* Displaying menu items */}
 
       <div>
--- src/Data.js
+++ src/Data.js
@@ -2,44 +2,56 @@ const menu = [
     {
         id: 1,
         name:'Bhajiya',
-        category: 'Dinner',
+        category1: 'Dinner',
+        category2: 'Non-Veg',
         price: 40,
+        orders: 1,
         // img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSESrQ7tequGJ_UtaOyoNMUNnqQNDWrDwpOtwcr_8Fm6g&s'
         img :'https://img.freepik.com/premium-photo/indian-traditional-paneer-pakora-snacks-white-background_55610-3883.jpg?w=2000'
     },
     {
         id:2,
         name: 'Bhakharvadi',
-        category: 'Breakfast',
+        category1: 'Breakfast',
+        category2: 'Non-Veg',
         price: 20,
+        orders: 2,
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiwDrvKcYV9gzNxyTQ8vpKVKi1BfJKdbRp3Q&usqp=CAU'
     },
     {
         id:3,
         name: 'Pulav',
-        category:'Dinner',
+        category1:'Dinner',
+        category2: 'Veg',
         price: 50,
+        orders: 3,
         img: 'https://img.freepik.com/premium-photo/indian-cuisine-pulao-white-background_55610-145.jpg?w=2000'
     },
     {
         id:4,
         name: 'Dabeli',
-        category: 'Lunch',
+        category1: 'Lunch',
+        category2: 'Veg',
         price: 35,
+        orders: 4,
         img: 'https://img.freepik.com/premium-photo/dabeli_57665-775.jpg'
     },
     {
         id:5,
         name: 'Fulwadi',
-        category: 'Breakfast',
+        category1: 'Breakfast',
+        category2: 'Veg',
         price: 25,
+        orders: 5,
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLnStk-cxHTt7XZDdwDIMkLQDMhJ-z4kYPag&usqp=CAU'
     },
     {
         id:6,
         name: 'Poha',
-        category: 'Lunch',
+        category1: 'Lunch',
+        category2: 'Non-Veg',
         price:30,
+        orders: 6,
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_56GYvSV-IXnQS5ZjX4mTUJ5xNJX6ge0CfA&usqp=CAU'
     }
 ];
